0146b1435c6d275ee481929e861a9f943b9b48e9
Jayden <j@Jaydens-MacBook-Pro.local>
2024-05-04 14:10:23 -0500

    Update TODO, starting checkpoints for 2.1 for stockfish"

--- README.md
+++ README.md
@@ -12,4 +12,6 @@ TODO:
 4. Change self play functionality
 5. Better logging 
 6. Store more data for visualization 
-
+7. Rewrite in rust 
+* Just needs MCTS and self play environment 
+* Adapt NNUE from [https://github.com/lovechants/projects/tree/main/neural_nets/nn_rust](rust nn) 
--- chess_v2_1/nnue.py
+++ chess_v2_1/nnue.py
@@ -81,9 +81,9 @@ def process_training_data(data, num_moves=4096):
     return inputs, policy_targets,value_targets
 
 def train_network(model, inputs, policy_targets, value_targets, epochs=20, batch_size=32): 
-    log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-    history = model.fit(inputs, [policy_targets, value_targets], epochs=epochs, batch_size=batch_size, verbose=1, callbacks=[tensorboard_callback])
+    #log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    history = model.fit(inputs, [policy_targets, value_targets], epochs=epochs, batch_size=batch_size, verbose=1)#, callbacks=[tensorboard_callback])
     return history
 
 
